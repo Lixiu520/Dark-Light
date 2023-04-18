83743d768d4c48fc78ab488bae2cc31820e98122
Lixiu520 <118369717+Lixiu520@users.noreply.github.com>
2023-04-18 08:38:07 +0200

    Add files via upload

---
+++ Alterner Entre Dark Mode Et Light Mode/script.js
@@ -0,0 +1,29 @@
+console.log('connecté!');
+console.log("Hello!");
+const switchBox= document.querySelector('.switch');
+console.log(switchBox);
+const btn = document.querySelector('.btn');
+console.log(btn);
+const icone=document.querySelector('i');
+console.log(icone);
+const container=document.querySelector('.container');
+console.log(container);
+const titre=document.querySelector('h1');
+console.log(titre);
+
+switchBox.addEventListener('click', function(){
+    console.log('DIV cliquée!');
+    btn.style.left="60px";
+    btn.classList.toggle('btn-change');
+    icone.classList.toggle('icone-change');
+    icone.classList.toggle('fa-sun');
+    switchBox.classList.toggle('switch-change');
+    container.classList.toggle('container-change');
+    // titre.innerText="LIGHT MODE";
+    if(titre.innerText=="DARK MODE"){
+        titre.innerText="LIGHT MODE";
+    }else{
+        titre.innerText="DARK MODE";
+    }
+});;
+
